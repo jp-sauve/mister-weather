fdddbdc2d14e7656bf73a8249c0318f48525469a
jp-sauve <jean-paul@grokology.ca>
2017-05-30 01:01:10 -0400

    create nav object

--- js/script.js
+++ js/script.js
@@ -2,10 +2,11 @@
 /*global $, jQuery*/
 $(document).ready(function () {
     "use strict";
-    if (navigator.hasOwnProperty('geolocation')) {
-        console.log(navigator);
+    var nav = window.navigator;
+    if (nav.hasOwnProperty('geolocation')) {
+        console.log(nav);
     } else {
         console.log("No geolocation in navigator.");
-        console.log(navigator);
+        console.log("This is " + nav);
     }
 });
