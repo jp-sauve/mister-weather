2d3483db4610b350d6df1c02f683b36afbe78fda
jp-sauve <jean-paul@grokology.ca>
2017-06-21 01:59:16 -0400

    restructure. change color. remove extra comments. show temp.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,37 @@
 /*jslint devel:true */
 /*global $, jQuery*/
 
+function showForecast(lat, long) {
+    "use strict";
+    var apiKey = "key=9251eadaf0d54c5aae630949172704",
+        myQuery = "q=" + lat + "," + long,
+        baseURL = "https://api.apixu.com/v1/current.json?" + apiKey + "&" + myQuery;
+    console.log("Showing Forecast");
+    $.ajax({type: 'GET',
+           dataType: 'json',
+           url: baseURL,
+           success: function (json) {
+            console.log(json);
+            var html = "";
+            html += json.current.temp_c;
+            $('#temp').html(html);
+        },
+            cache: false
+           });
+}
+
 function showPosition(position) {
     "use strict";
-    console.log(position);
     var myLatitude = position.coords.latitude,
         myLongitude = position.coords.longitude,
         myAccuracy = position.coords.accuracy;
-    $('#geo-report').html("Latitude: " + myLatitude + "  -=-  Longitude: " + myLongitude + "<br/>Accuracy: " + myAccuracy);
+    showForecast(myLatitude, myLongitude);
+    $('#myLatitude').html(myLatitude);
+    $('#myLongitude').html(myLongitude);
+    $('#myAccuracy').html(myAccuracy);
 }
+
+
 
 $(document).ready(function () {
     "use strict";
@@ -19,5 +42,4 @@ $(document).ready(function () {
     } else {
         alert("No geolocation in navigator.");
     }
-
 });
