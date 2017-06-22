934ba3ec44204af68acbb178032a83a39aeccdc8
jp-sauve <jean-paul@grokology.ca>
2017-06-22 19:47:45 -0400

    fix FF on Linux failing silently

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 
 function showForecast(position) {
     "use strict";
+    console.log("Showing Forcast begins...");
     var myLatitude = position.coords.latitude,
         myLongitude = position.coords.longitude,
         myAccuracy = position.coords.accuracy,
@@ -13,7 +14,7 @@ function showForecast(position) {
     $('#myLongitude').html(myLongitude);
     $('#myAccuracy').html(myAccuracy);
 
-    console.log("Showing Forecast");
+    console.log("Showing Forecast AJAX begins...");
     $.ajax({type: 'GET',
            dataType: 'json',
            url: baseURL,
@@ -28,15 +29,39 @@ function showForecast(position) {
             cache: false
            });
 }
+function err(err) {
+    console.log("Error Report:");
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+    console.log(err);
 
+};
 
 
 $(document).ready(function () {
     "use strict";
-    var nav = window.navigator;
+    console.log("Document Ready...");
+    var nav = window.navigator,
+        browserChrome = null;
     if (nav.geolocation) {
-        console.log(nav);
-        nav.geolocation.getCurrentPosition(showForecast);
+
+        // Following section fixes FF53.0.3 Linux not returning position object
+        if (!!window.chrome) {
+        var options = {
+            enableHighAccuracy: true,
+            timeout: 30000,
+            maximumAge: 0
+        };
+        } else {
+            var options = {
+                enableHighAccuracy: false,
+                timeout: 0,
+                maximumAge: Infinity
+            };
+            // FF fix finishes here
+        }
+        console.log("Geolocate Exists");
+     //   nav.geolocation.getCurrentPosition(function () {}, function () {}, {});
+        nav.geolocation.getCurrentPosition(showForecast, err, options);
     } else {
         alert("No geolocation in navigator.");
     }
