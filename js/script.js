55bee6262d15b3c241a6ec3a1ff51b59154f457d
jp-sauve <jean-paul@grokology.ca>
2017-06-25 04:15:57 -0400

    fix lint
    update style
    set icon using conditions.json

--- js/script.js
+++ js/script.js
@@ -11,15 +11,20 @@ function showForecast(position) {
         this.myLatitude = position.coords.latitude;
         this.myLongitude = position.coords.longitude;
         this.myAccuracy = position.coords.accuracy;
-        this.conditions = $.getJSON("/mister-weather/data/conditions.json");
+        this.iconCode = "";
+        this.conditionText = "unknown";
+        this.is_day = 0;
         this.tempUnit = "c";
         this.apiKey = "key=9251eadaf0d54c5aae630949172704";
         this.myQuery = "q=" + this.myLatitude + "," + this.myLongitude;
-        this.imgURLbase = "/mister-weather/data/weather/64x64/";
+        this.imgURLbase = "/data/weather/64x64/";
         this.baseURL = "https://api.apixu.com/v1/current.json?" + this.apiKey + "&" + this.myQuery;
     })();
+    $.getJSON("/data/conditions.json", function (data) {
+        dataObj.conditions = data;
+    });
 // Get weather using Apixu API
-    console.log("Showing Forecast AJAX begins...");
+    console.log("Weather AJAX begins...");
     $.ajax({type: 'GET',
            dataType: 'json',
            url: dataObj.baseURL,
@@ -30,34 +35,51 @@ function showForecast(position) {
 
 function updateForecast(json) {
     "use strict";
-    console.log(JSON.stringify(json));
     dataObj.json = json;
-    var html = "",
-        loc_html = json.location.name,
-        curr_temp = (parseFloat((dataObj.tempUnit === "c") ? json.current.temp_c : json.current.temp_f)),
- /*       icon_num = $.each(dataObj.conditions,function(key,val){
-            $.each(val,function(key1,val1) {
-            console.log("IconNum: "+key1+ " " + val1);
+    //console.log(JSON.stringify(json));
+/*
+    console.log(json.location.name,json.current.condition.icon);
+    let keys = Object.keys(json.current);
+    console.log(keys);
+    keys.forEach(function(key) {
+        console.log('key: '+key+' value: '+json.current[key]);
+    });
+ */
+    var conditionObjects = Object.keys(dataObj.conditions);
+ //   console.log("ConditionObjects: "+JSON.stringify(conditionObjects));
+    conditionObjects.forEach(function (key) {
+        if ((dataObj.conditions[key].code) === (dataObj.json.current.condition.code)) {
+ //           console.log("FOUND MATCH: " + dataObj.json.current.condition.code);
+            dataObj.iconCode = dataObj.conditions[key].icon;
+            if (dataObj.json.current.condition.is_day) {
+                dataObj.conditionText = dataObj.conditions[key].day;
+            } else {
+                dataObj.conditionText = dataObj.conditions[key].night;
+            }
 
-            })
-        });
-    */
-    curr_icon = dataObj.imgURLbase + ((dataObj.json.current.condition.is_day) ? "day/" : "night/") + "113.png";
-        //    curr_temp = 99.99;
-    console.log("curr_temp: " + curr_temp);
+        }
+    });
+    var html = "",
+        loc_html = dataObj.json.location.name,
+        curr_temp = (parseFloat((dataObj.tempUnit === "c") ? dataObj.json.current.temp_c : dataObj.json.current.temp_f)),
+        curr_icon = dataObj.imgURLbase + ((dataObj.json.current.condition.is_day) ? "day/" : "night/") + dataObj.iconCode + ".png";
+        //TESTING OVERRIDE
+        //curr_temp = 99.99;
     var curr_temp_left = (function () {
         return Math.trunc(curr_temp);
     }());
-    console.log("curr_temp_left: " + curr_temp_left);
+
 
     // Make curr_temp_right a whole number based on the fractional part of curr_temp. e.g. curr_temp = 1.7 then curr_temp_right = 7
     var curr_temp_right = ((curr_temp - curr_temp_left) * 10).toFixed(0);
-    console.log("curr_temp_right: " + curr_temp_right);
 
+    $('span#condition-text').html(dataObj.conditionText);
+
+    $('span#time-phrase').html((dataObj.json.current.condition.is_day ? "today" : "tonight"));
     $('button#toggleUnit #other-unit').html((dataObj.tempUnit === "c") ? "F" : "C");
     $('#curr_temp_left').html(curr_temp_left);
     $('#curr_temp_right').html("." + ('0' + curr_temp_right).slice(-1));
-    $('img#weatherIcon').attr("src",curr_icon);
+    $('img#weatherIcon').attr("src", curr_icon);
     $('#myCity').html(loc_html);
     $('#myLatitude').html(dataObj.myLatitude);
     $('#myLongitude').html(dataObj.myLongitude);
